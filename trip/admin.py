1647f1a17d544674bf16c091dda286dfc44739ff
zishan344 <zishanahmed344@gmail.com>
2026-01-28 20:47:04 +0600

    register modle

--- trip/admin.py
+++ trip/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Trip, RouteStop,DailyLog,LogSegment
 # Register your models here.
+admin.site.register(Trip)
+admin.site.register(RouteStop)
+admin.site.register(DailyLog)
+admin.site.register(LogSegment)
