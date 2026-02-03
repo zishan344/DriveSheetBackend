e513cee774d088b0a87cb6085d5369a547633378
zishan344 <zishanahmed344@gmail.com>
2026-02-03 19:28:45 +0600

    Fix build script pip command

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python3.9 -m pip install -r requirements.txt --break-system-packages || pip install -r requirements.txt
-python manage.py collectstatic --noinput --clear
+pip install -r requirements.txt
+python manage.py collectstatic
