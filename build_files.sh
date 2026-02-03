7e6d0da7447ef8ab30c03a7276bdceef307860c0
zishan344 <zishanahmed344@gmail.com>
2026-02-03 19:25:36 +0600

    Fix pip install error on Vercel

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt --break-system-packages || pip install -r requirements.txt
 python manage.py collectstatic --noinput --clear
